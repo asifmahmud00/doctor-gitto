aeedb064672735cb3841510f917818941f77657b
asifmahmud00 <asif.mahmud00@gmail.com>
2023-06-19 12:25:13 +0600

    second pc commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <button class="btn">Bismillah</button>
       <button class="btn btn-primary">Subhanallah</button>
       <button class="btn btn-secondary">Alhamdulillah</button>
-      <button class="btn btn-accent">L a Ilaha Illallah</button>
+      <button class="btn btn-accent">La Ilaha Illallah</button>
       <button class="btn btn-ghost">Allhu Akbar</button>
       <button class="btn btn-link">Wa lillahil Hamd</button>
     </div>
